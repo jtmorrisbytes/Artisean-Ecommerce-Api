07756ca1c58d7c23bf012453db495e532330098b
= <=>
2020-03-28 01:18:16 -0500

    almost done implementing get one

--- src/routes/products/get/one.js
+++ src/routes/products/get/one.js
@@ -1,6 +1,24 @@
+const model = require("../../../../lib/src/models/product");
+const modelInfo = model.modelInfo;
+const data = require("../data.json");
 module.exports = {
   controller: function getOne(req, res) {
-    res.send(501);
+    if (!req.params.id) {
+      // if the parameter is missing from the request
+      // return 422 (unproccessable entity)
+      res.sendStatus(422);
+    }
+    if (!modelInfo.id.format.test(req.params.id)) {
+      // the parameter was provided, but the type was incorrect
+      res.sendStatus(400);
+    }
+    let result = data.find(product => {
+      return product.id == req.params.id;
+    });
+    if (!result) {
+      res.sendStatus(404);
+    }
+    res.json([result]);
   },
   params: {
     request: ":id",
