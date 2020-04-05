04a262bff2b1676b3c63531ad6c56b937978c286
= <=>
2020-04-05 02:29:37 -0500

    done for today

--- client/src/controllers/Navigation/Navigation.js
+++ client/src/controllers/Navigation/Navigation.js
@@ -1,7 +1,7 @@
 import React from "react";
-import "./Navigation.css";
+import "./Navigation.scss";
 import { Link } from "react-router-dom";
-const Navigation = props => {
+const Navigation = (props) => {
   return (
     <nav className="Navigation">
       <div className="nav-links">
