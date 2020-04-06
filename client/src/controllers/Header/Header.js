b49aff2b54e99ab42f8bde9f248462066d2d6e36
= <=>
2020-04-05 21:39:28 -0500

    merge header components into single component

--- client/src/controllers/Header/Header.js
+++ client/src/controllers/Header/Header.js
@@ -1,18 +1,48 @@
 import React from "react";
 import Navigation from "../Navigation/Navigation";
 import Search from "../Search/Search";
+import { Link } from "react-router-dom";
 import "./Header.scss";
-const Header = props => {
-  return (
-    <header className="Header">
-      <img
-        src="http://picsum.photos/500/200"
-        className="brand-img"
-        alt="brand image"
-      />
-      <Navigation />
-      <Search />
-    </header>
-  );
-};
+class Header extends React.Component {
+  state = {
+    searchText: "",
+  };
+  render() {
+    return (
+      <header className="Header">
+        <div className="row">
+          <img
+            src="http://picsum.photos/500/200"
+            className="brand-img"
+            alt="brand image"
+          />
+          <nav className="Navigation">
+            <div className="nav-links">
+              <Link to="/home">HOME</Link>
+              <Link to="/products">PRODUCTS</Link>
+            </div>
+            <div className="cart-avatar">
+              <Link to="/cart">CART</Link>
+            </div>
+          </nav>
+        </div>
+        <div className="row">
+          <div className="Search">
+            <input
+              type="text"
+              value={this.state.searchText}
+              placeholder="search for a product by name"
+              onChange={(e) => {
+                this.setState({ searchText: e.target.value });
+              }}
+            />
+            <Link role="button" to={"/products?name=" + this.state.searchText}>
+              Search
+            </Link>
+          </div>
+        </div>
+      </header>
+    );
+  }
+}
 export default Header;
